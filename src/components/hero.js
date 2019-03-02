af8254427f0780679c70401daad6bcfb50a1793b
Damien Senger <damien@raccoon.studio>
2019-03-02 23:52:21 +0100

    Installs and use uuid package for HTML unique Ids

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Styles from "../styles/components/hero.module.scss"
+import uuid from "uuid/v1"
 
-const Hero = ({ salutations }) => (
-  <section className={Styles.hero}>
+const Hero = ({ salutations, componentId }) => (
+  <section className={Styles.hero} aria-labelledby={componentId + "_title"}>
     <div className={Styles.hero__container}>
       <h1 className={Styles.hero__title}>
         <span className={Styles.hero__salutations}>
@@ -11,8 +12,10 @@ const Hero = ({ salutations }) => (
             <span lang={item.lang}>{item.content}</span>
           ))}
         </span>
-        I am <strong>Damien</strong>, digital <strong>designer</strong> and{" "}
-        <strong>accessibility advocate</strong>.
+        <span id={componentId + "_title"}>
+          I am <strong>Damien</strong>, digital <strong>designer</strong> and{" "}
+          <strong>accessibility advocate</strong>.
+        </span>
       </h1>
     </div>
   </section>
@@ -20,10 +23,12 @@ const Hero = ({ salutations }) => (
 
 Hero.propTypes = {
   salutations: PropTypes.array,
+  componentId: PropTypes.string,
 }
 
 Hero.defaultProps = {
   salutations: null,
+  componentId: uuid(),
 }
 
 export default Hero
