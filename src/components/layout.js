de08b781a98af37d6f457df592b5600558cd9c87
Damien Senger <damien@raccoon.studio>
2019-03-02 20:14:45 +0100

    Replaces Gatsby layout.css for normalize.css
    Normalize.css is used to replace layout.css. Normalize.css was
    integrated to the `src/styles/global.scss` with the `html` and `body`
    changes needed to keep the `1rem` = `10px`.

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -29,22 +29,16 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} mainNavigation={data.site.siteMetadata.mainNavigation} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+          mainNavigation={data.site.siteMetadata.mainNavigation}
+        />
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </>
     )}
   />
