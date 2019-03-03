0ab0f4a2221406f5b00c03a945a871d960107532
Damien Senger <damien@raccoon.studio>
2019-03-03 14:16:49 +0100

    Adds a new image block after the Hero block

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,24 +1,51 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Styles from "../styles/components/hero.module.scss"
+import Img from "gatsby-image"
 import uuid from "uuid/v1"
+import { StaticQuery, graphql } from "gatsby"
 
 const Hero = ({ salutations, componentId }) => (
-  <section className={Styles.hero} aria-labelledby={componentId + "_title"}>
-    <div className={Styles.hero__container}>
-      <h1 className={Styles.hero__title}>
-        <span className={Styles.hero__salutations}>
-          {salutations.map(item => (
-            <span lang={item.lang}>{item.content}</span>
-          ))}
-        </span>
-        <span id={componentId + "_title"}>
-          I am <strong>Damien</strong>, digital <strong>designer</strong> and{" "}
-          <strong>accessibility advocate</strong>.
-        </span>
-      </h1>
-    </div>
-  </section>
+  <StaticQuery
+    query={graphql`
+      query {
+        file(relativePath: { eq: "damiensenger.jpg" }) {
+          childImageSharp {
+            # Specify the image processing specifications right in the query.
+            # Makes it trivial to update as your page's design changes.
+            fixed(width: 1280, height: 720) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <section
+          className={Styles.hero}
+          aria-labelledby={componentId + "_title"}
+        >
+          <div className={Styles.hero__container}>
+            <h1 className={Styles.hero__title}>
+              <span className={Styles.hero__salutations}>
+                {salutations.map(item => (
+                  <span lang={item.lang}>{item.content}</span>
+                ))}
+              </span>
+              <span id={componentId + "_title"}>
+                I am <strong>Damien</strong>, digital <strong>designer</strong>{" "}
+                and <strong>accessibility advocate</strong>.
+              </span>
+            </h1>
+          </div>
+        </section>
+        <section className={Styles.heroImage}>
+          <Img fixed={data.file.childImageSharp.fixed} />
+        </section>
+      </>
+    )}
+  />
 )
 
 Hero.propTypes = {
