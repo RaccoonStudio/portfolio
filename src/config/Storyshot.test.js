f960c092b8883ded8f99b9b9ec21f16279478073
Damien Senger <damien@raccoon.studio>
2019-06-20 23:16:03 +0200

    Install Storyshot Images

--- src/config/Storyshot.test.js
+++ src/config/Storyshot.test.js
@@ -1,3 +1,19 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-initStoryshots();
+const customizePage = page => {
+  return page.emulate({
+    'name': 'Desktop 1920x1080',
+    'userAgent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+    'viewport': {
+      'width': 1920,
+      'height': 1080,
+      'deviceScaleFactor': 2,
+      'isMobile': false,
+      'hasTouch': false,
+      'isLandscape': true
+    }
+  });
+};
+
+initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({ customizePage }) });
